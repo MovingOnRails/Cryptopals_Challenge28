e805dd1197b892d38cea93f1007cca4c7dd7537f
first <first@windows.windows>
2026-03-26 18:36:49 -0300

    Authenticate functions works correctly

--- main.c
+++ main.c
@@ -1,31 +1,34 @@
 #include <openssl/sha.h>
+
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
-
+#include <stdlib.h>
 
 // Assumes key is of length 16 bytes
-unsigned char getSecretPrefixMAC_SHA1(unsigned char* key, int keysize, unsigned char* text, int textLength){
-    size_t length = keysize+textLength;
+unsigned char* getSecretPrefixMAC_SHA1(unsigned char* key, int keylength,
+                                      unsigned char* text, int textLength){
+    size_t length = keylength+textLength;
     unsigned char* hash = malloc(SHA_DIGEST_LENGTH);
-    size_t datasize = keysize + textLength;
+    size_t datasize = keylength + textLength;
     unsigned char* data = malloc(datasize);
-    memcpy(data,key,keysize);
-    memcpy(data+keysize,text,textLength);
+    memcpy(data,key,keylength);
+    memcpy(data+keylength,text,textLength);
     SHA1(data, datasize, hash);
 
     // Print the hash as a hexadecimal string
-    for (int i = 0; i < SHA_DIGEST_LENGTH; i++) {
+    /*for (int i = 0; i < SHA_DIGEST_LENGTH; i++) {
         printf("%02x", hash[i]);
     }
-    printf("\n");
+    printf("\n");*/
 
+    free(data);
     return hash;
 }
 
-
-int main() {
-    //unsigned char* data = getSecretPrefixMAC_SHA1(NULL,0,"Hello, world!", 13);
-    unsigned char* data = getSecretPrefixMAC_SHA1("Secret sauce",12,"Hello, world!", 13);
-    
-    return 0;
+bool authenticate(unsigned char* key, int keylength, 
+                  unsigned char* message, int messageLength, 
+                  unsigned char* SHA1Digest){
+    unsigned char* generatedDigest = getSecretPrefixMAC_SHA1(key, keylength, message, messageLength);
+    return memcmp(generatedDigest, SHA1Digest,SHA_DIGEST_LENGTH) == 0;
 }
---
+++ test.c
@@ -0,0 +1,16 @@
+#include "./main.c"
+
+
+int main() {
+    unsigned char key[13] = "Secret sauce";
+    int keylength = 12;
+    unsigned char text[14] = "Hello, world!";
+    int textLength = 13;
+    unsigned char* data = getSecretPrefixMAC_SHA1("Secret sauce",12,"Hello, world!", 13);
+    bool correctDigest = authenticate(key, keylength,text, textLength,data);
+    
+    
+    bool messageTamperedDigest = authenticate(key, keylength, "Bye bye, world!",15, data);
+    bool keyTamperedDigest = authenticate("Ultrasecret sauce", 17, text, textLength, data);
+    return 0;
+}
